dad1417f0d28e41efe10ab7ae5998bd5b962056c
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2023-11-30 23:59:10 +0900

    Add navigation actions for signed in and user created states

--- lib/src/screens/custom_sign_in_screen.dart
+++ lib/src/screens/custom_sign_in_screen.dart
@@ -1,7 +1,9 @@
+import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
 import 'package:faker_app_flutter_firebase/src/screens/ui_auth_providers.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomSignInScreen extends ConsumerWidget {
   const CustomSignInScreen({super.key});
@@ -15,6 +17,14 @@ class CustomSignInScreen extends ConsumerWidget {
       ),
       body: SignInScreen(
         providers: authProviders,
+        actions: [
+          AuthStateChangeAction<SignedIn>((context, state) {
+            context.goNamed(AppRoute.profile.name);
+          }),
+          AuthStateChangeAction<UserCreated>((context, state) {
+            context.goNamed(AppRoute.profile.name);
+          })
+        ],
       ),
     );
   }
