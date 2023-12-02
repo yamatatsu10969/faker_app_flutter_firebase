3fced52e25fd05e22b2f1b95075e657b7b82986e
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2023-12-02 16:17:44 +0900

    Implement SignedOutAction

--- lib/src/screens/custom_profile_screen.dart
+++ lib/src/screens/custom_profile_screen.dart
@@ -1,7 +1,9 @@
+import 'package:faker_app_flutter_firebase/src/routing/app_router.dart';
 import 'package:faker_app_flutter_firebase/src/screens/ui_auth_providers.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomProfileScreen extends ConsumerWidget {
   const CustomProfileScreen({super.key});
@@ -14,6 +16,11 @@ class CustomProfileScreen extends ConsumerWidget {
         title: const Text('Profile'),
       ),
       providers: authProviders,
+      actions: [
+        SignedOutAction((context) {
+          context.goNamed(AppRoute.signIn.name);
+        })
+      ],
     );
   }
 }
